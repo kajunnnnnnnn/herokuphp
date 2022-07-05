08fd4f85e382e3554a106a1cdd886c63f724b8ae
kajunnnnnnnn <20026955@myrp.edu.sg>
2022-07-05 12:42:59 +0800

    commit message goes here

--- web/test.php
+++ web/test.php
@@ -3,7 +3,7 @@
 <html>
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Hello World</title>
 </head>
 <body>
